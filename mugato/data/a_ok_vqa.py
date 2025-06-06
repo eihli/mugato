Fix bug related to masking
Refactor some of the code related to the "infinite dataloader"
so that the utility function simply calls the function defined
by each dataset, because the dataset itself knows
which keys need to be masked.

mugato/data/a_ok_vqa.py
@@ -3,7 +3,7 @@ from functools import partial
 import random
 import torch
 from torch.utils.data import DataLoader, Dataset
-from mugato.data.utils import splits
+from mugato.data.utils import splits, infinite_dataloader
 from mugato.utils import (
     Timesteps,
     TransformDataset,
@@ -12,6 +12,9 @@ from mugato.utils import (
     as_tensor,
 )
 from datasets import load_dataset
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def example():
@@ -47,6 +50,7 @@ def initialize():
 
 
 def tokenize(tokenizer, sample):
+    logger.debug(f"Tokenizing a_ok_vqa.")
     question = [tokenizer.encode_text(sample["question"])]
     image = [tokenizer.encode_image(image_transform(as_tensor(sample["image"])))]
     eot = torch.tensor([[tokenizer.eot_token_id]])
@@ -94,7 +98,18 @@ def tokenize(tokenizer, sample):
     return xs, ys
 
 
-def create_dataloader(tokenizer, batch_size, split="train"):
+def create_dataloader(tokenizer, batch_size, split="train", block_size=1024):
     dataset = initialize()
     dataset = TransformDataset(dataset[split], partial(tokenize, tokenizer))
-    return DataLoader(dataset, batch_size=batch_size, collate_fn=generic_collate_fn)
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        collate_fn=partial(generic_collate_fn, sequence_length=block_size, mask_keys=["answer"]),
+    )
+
+def create_infinite_dataloader(tokenizer, batch_size, split="train", block_size=1024):
+    dataset = initialize()
+    dataset = TransformDataset(dataset[split], partial(tokenize, tokenizer))
+    return infinite_dataloader(
+        partial(DataLoader, dataset, batch_size=batch_size, collate_fn=partial(generic_collate_fn, sequence_length=block_size, mask_keys=["answer"]))
+    )

mugato/data/pointmaze.py
@@ -3,6 +3,7 @@ import numpy as np
 import minari
 import torch
 from torch.utils.data import DataLoader
+from mugato.data.utils import infinite_dataloader
 from mugato.utils import (
     Timesteps,
     TransformDataset,
@@ -75,7 +76,19 @@ def tokenize(tokenizer, sample):
     return xs, ys
 
 
-def create_dataloader(tokenizer, batch_size, split="train"):
+def create_dataloader(tokenizer, batch_size, split="train", block_size=1024):
     dataset = initialize()
     dataset = TransformDataset(dataset[split], partial(tokenize, tokenizer))
-    return DataLoader(dataset, batch_size=batch_size, collate_fn=generic_collate_fn)
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        collate_fn=partial(generic_collate_fn, sequence_length=block_size, mask_keys=["action"]),
+    )
+
+
+def create_infinite_dataloader(tokenizer, batch_size, split="train", block_size=1024):
+    dataset = initialize()
+    dataset = TransformDataset(dataset[split], partial(tokenize, tokenizer))
+    return infinite_dataloader(
+        partial(DataLoader, dataset, batch_size=batch_size, collate_fn=partial(generic_collate_fn, sequence_length=block_size, mask_keys=["action"]))
+    )

mugato/data/shakespeare.py
@@ -4,7 +4,7 @@ import re
 import requests
 import torch
 from torch.utils.data import DataLoader
-from mugato.data.utils import splits
+from mugato.data.utils import splits, infinite_dataloader
 from mugato.utils import Timesteps, data_home, TransformDataset, generic_collate_fn
 
 
@@ -61,7 +61,19 @@ def tokenize(tokenizer, sample):
     return xs, ys
 
 
-def create_dataloader(tokenizer, batch_size, split="train"):
+def create_dataloader(tokenizer, batch_size, split="train", block_size=1024):
     dataset = initialize()
     dataset = TransformDataset(dataset[split], partial(tokenize, tokenizer))
-    return DataLoader(dataset, batch_size=batch_size, collate_fn=generic_collate_fn)
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        collate_fn=partial(generic_collate_fn, sequence_length=block_size, mask_keys=["text"]),
+    )
+
+
+def create_infinite_dataloader(tokenizer, batch_size, split="train", block_size=1024):
+    dataset = initialize()
+    dataset = TransformDataset(dataset[split], partial(tokenize, tokenizer))
+    return infinite_dataloader(
+        partial(DataLoader, dataset, batch_size=batch_size, collate_fn=partial(generic_collate_fn, sequence_length=block_size, mask_keys=["text"]))
+    )

mugato/utils.py
@@ -85,7 +85,9 @@ class TransformDataset(Dataset):
         return self.transform(self.dataset[idx])
 
 
-def generic_collate_fn(batch, sequence_length=1024):
+def generic_collate_fn(batch, sequence_length=1024, mask_keys=None):
+    if mask_keys is None:
+        mask_keys = []
     sliced = [
         (
             slice_to_context_window(sequence_length, xs),
@@ -96,7 +98,7 @@ def generic_collate_fn(batch, sequence_length=1024):
     # sliced is a (B, 2, ...) list.
     # the 2 is xs, ys
     xs, ys = [v for v in zip(*sliced)]
-    xs, ys, ms = pad(xs), pad(ys), mask(ys)
+    xs, ys, ms = pad(xs), pad(ys), mask(ys, mask_keys)
     return xs, ys, ms
 
 
@@ -160,14 +162,15 @@ def pad(batch, padding_value=0):
     return Timesteps([(k, torch.stack(v)) for k, v in padded.items()])
 
 
-def mask(batch):
+def mask(batch, mask_keys):
     result = Timesteps()
     for k, v in batch[0].items():
         episode_lengths = [sample[k].size(0) for sample in batch]
         token_lengths = [sample[k].size(1) for sample in batch]
         result[k] = torch.zeros(len(batch), max(episode_lengths), max(token_lengths))
         for i in range(len(batch)):
-            result[k][i][: episode_lengths[i], : token_lengths[i]] = 1
+            if k in mask_keys:
+                result[k][i][:episode_lengths[i], :token_lengths[i]] = 1
     return result
 
 